43de84288787c85d8404b55e499935b6697e3173
barlingo <16936512+barlingo@users.noreply.github.com>
2022-06-19 17:52:21 -0400

    [feat] added pixel to point conversion

--- chapter2/madelbrot/src/main.rs
+++ chapter2/madelbrot/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 /// origin. If 'c' seems to be a member (more precisely, if we reached the iteration
 /// limit without being able to prove that 'c' is not a member),
 /// return 'None'.
-fn ecape_time(c: Complex<f64>, limit: u64) -> Option<u64> {
+fn escape_time(c: Complex<f64>, limit: u64) -> Option<u64> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
@@ -49,6 +49,28 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
     //     Some((re, im)) => Some(Complex { re, im }),
     //     None => None,
     // }
+}
+
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+/// 'bounds' is a pair giving the width and height of the image in pixels
+/// 'pixel' is a (column, row) pair indicating a particular pixel in that image.
+/// The 'upper_left' and 'lower_right' parameters are points on the complex plane
+/// designating the area our image covers.
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    }
 }
 
 #[cfg(test)]
@@ -104,5 +126,20 @@ mod tests {
     #[test]
     fn test_parse_pair_complex_missing_field2() {
         assert_eq!(parse_complex("1.0,"), None)
+    }
+    #[test]
+    fn test_pixel_to_point() {
+        assert_eq!(
+            pixel_to_point(
+                (100, 200),
+                (25, 175),
+                Complex { re: -1.0, im: 1.0 },
+                Complex { re: 1.0, im: -1.0 }
+            ),
+            Complex {
+                re: -0.5,
+                im: -0.75
+            }
+        );
     }
 }
